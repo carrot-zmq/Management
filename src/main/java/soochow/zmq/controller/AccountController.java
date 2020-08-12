14f2929c4cf9dc48d2370622fa6ba11e07ed27a7
zmq <123@qq.com>
2020-08-12 16:38:17 +0800

    zmq_management

---
+++ src/main/java/soochow/zmq/controller/AccountController.java
@@ -0,0 +1,16 @@
+package soochow.zmq.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import soochow.zmq.service.UserManageService;
+
+/*@RestController
+public class AccountController {
+
+    @Autowired
+    UserManageService userManageService;
+
+    @Autowired
+    JwtUtils jwtUtils;
+}*/
